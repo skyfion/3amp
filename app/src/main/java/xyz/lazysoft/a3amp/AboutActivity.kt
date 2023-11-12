95d750cbb70436693a7e79e7ffa67febd7bdab0f
Alexandr Kondratyev <sky.fion@gmail.com>
2023-11-12 14:40:47 +0300

    add support donation

--- app/src/main/java/xyz/lazysoft/a3amp/AboutActivity.kt
+++ app/src/main/java/xyz/lazysoft/a3amp/AboutActivity.kt
@@ -1,9 +1,15 @@
 package xyz.lazysoft.a3amp
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+
 
 class AboutActivity : AppCompatActivity() {
 
@@ -22,21 +28,16 @@ class AboutActivity : AppCompatActivity() {
         val titleAbout = findViewById<TextView>(R.id.title_about)
 
         titleAbout.text = "${getString(R.string.app_name)} ${getVersion()}"
-    }
 
-    companion object {
+        val buyMeCoffee = findViewById<Button>(R.id.button_buy_coffee)
 
-        /**
-         * Google
-         */
-        const val GOOGLE_PUBKEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxGOZ1xt4RKuFoY2UBFO9JhuSi1Is2/QB91IVxSp+Y5lG0HgLwcjt8hfn1CTPp5yj3RLduufYALtV+xcbjH87ETrWkPez1O3nt7+SF6I/PIXF4dWdbC1QiVSHls4PWFqAghZeDMEhPH4qVayJglz/oBgnRL5X4cDErir6tGtYrDZWZz7AWVxWsfYJDIzlUPklQtEyxpYDlNybCEJzVrOGam/TYTRr3Qs2gjeUi61Uehkh1Yy/wDzvwz7ZCr2zggw2TcqN4WV1Zh6AkzZfjtuHYv9sz4qQWO6hMDiJfxRluHBTw2e1oX9d02t8RCiIRMrrQiYwTYVLzO6EN1JdFUHm9QIDAQAB"
-        val GOOGLE_CATALOG = arrayOf(
-                "donation.1",
-                "donation.2",
-                "donation.3",
-                "donation.5",
-                "donation.8",
-                "donation.13")
+        buyMeCoffee.setOnClickListener {
+            val url = "https://www.buymeacoffee.com/skyfi"
+            val i = Intent(Intent.ACTION_VIEW)
+            i.data = Uri.parse(url)
+            startActivity(i)
+        }
+
     }
 
 
